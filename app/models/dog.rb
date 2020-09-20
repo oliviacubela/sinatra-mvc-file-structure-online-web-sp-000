21029869710fa5aa80481d9cdb08c1d69c2263f6
Olivia Cubela <oliviacubela@gmail.com>
2020-09-20 19:59:52 +0000

    Automatically backed up by Learn

---
+++ app/models/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  attr_accessor :name, :breed, :age
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def save 
+    @@all << self
+  end
+     
+end
